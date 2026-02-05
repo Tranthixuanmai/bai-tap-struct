116e1f8e31ff3a3bd27d09b0c5aa906c57c15f39
Xuanmai <119525009@st.tvu.edu.vn>
2026-02-05 08:05:10 +0700

    update file

---
+++ baitapmatrix.c
@@ -0,0 +1,24 @@
+#define size 5
+
+typedef struct MA_TRAN {
+	int a[size][size];
+	int d, c; 
+}MT;
+
+int main ()
+{
+	void nhap(MT *a); //truyen tham bien
+	void xuat(MT a); //truyen tham tri
+	
+	return 0;
+}
+
+void nhap(MT *a) 
+{
+	
+}
+
+void xuat(MT a) 
+{
+	
+}
